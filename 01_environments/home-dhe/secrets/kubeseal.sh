efec7aa834b4fc7b2f907e413660501026c641c4
Daniel Hendricken <daniel.hendricken@bedag.ch>
2024-12-07 09:30:40 +0100

    chore: preparing repository for automatic bootstrapping of projects

--- 01_environments/home-dhe/secrets/kubeseal.sh
+++
@@ -1,4 +0,0 @@
-kubeseal --format=yaml \
-  --cert=/home/dan/Git/kubelize/kube-projects/01_environments/home-dhe/secrets/pub-home-dhe-sealed-secrets.pem \
-  --secret-file /home/dan/Git/kubelize/kube-projects/00_base/incubator/keycloak/secret-postgres.yaml \
-  --sealed-secret-file /home/dan/Git/kubelize/kube-projects/00_base/incubator/keycloak/keycloak-database-credentials.yaml
