cfebe316a88aa42a211bf9d908654b6491f393b3
Daniel Hendricken <daniel.hendricken@bedag.ch>
2024-12-13 11:00:37 +0100

    fix: kubeseal command for home-mck

--- environments/home-mck/inventory/secrets/sealed-secrets/kubeseal.sh
+++ environments/home-mck/inventory/secrets/sealed-secrets/kubeseal.sh
@@ -9,6 +9,6 @@ kubeseal --format=yaml \
 
 # curseforge-api-key
 kubeseal --format=yaml \
-  --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-dhe/pub-home-dhe-sealed-secrets.pem \
+  --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-mck/pub-home-mck-sealed-secrets.pem \
   --secret-file /home/dan/Git/kubelize/kube-projects/environments/home-dhe/inventory/secrets/secret.yaml \
   --sealed-secret-file /home/dan/Git/kubelize/kube-projects/environments/home-mck/inventory/secrets/cf-api-token.yaml
