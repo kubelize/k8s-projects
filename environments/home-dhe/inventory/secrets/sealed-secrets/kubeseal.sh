94d314856c7ab5634dc010bb877199eaa8a8a6c2
Daniel Hendricken <daniel.hendricken@bedag.ch>
2024-12-18 18:33:43 +0100

    chore: attempting to fix my stupidness

--- environments/home-dhe/inventory/secrets/sealed-secrets/kubeseal.sh
+++ environments/home-dhe/inventory/secrets/sealed-secrets/kubeseal.sh
@@ -4,7 +4,7 @@ kubeseal --fetch-cert --controller-namespace=sealed-secrets > /home/dan/Git/kube
 # minecraft rcon password
 kubeseal --format=yaml \
   --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-dhe/pub-home-dhe-sealed-secrets.pem \
-  --secret-file /home/dan/Git/kubelize/kube-projects/environments/home-dhe/inventory/secrets/secret.yaml \
+  --secret-file /home/dan/Git/kubelize/kube-projects/environments/home-dhe/inventory/secrets/secret-minecraft-rcon.yaml \
   --sealed-secret-file /home/dan/Git/kubelize/kube-projects/environments/home-dhe/inventory/secrets/minecraft-rcon-password.yaml
 
 # curseforge-api-key
