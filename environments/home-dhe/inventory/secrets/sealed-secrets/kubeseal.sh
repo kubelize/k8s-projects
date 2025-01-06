f8e86b01dd81276590b907690d0f1afb0cdd4a5f
Daniel Hendricken <daniel.hendricken@bedag.ch>
2025-01-06 20:01:20 +0100

    chore: testing nextcloud on home-dhe

--- environments/home-dhe/inventory/secrets/sealed-secrets/kubeseal.sh
+++ environments/home-dhe/inventory/secrets/sealed-secrets/kubeseal.sh
@@ -12,3 +12,15 @@ kubeseal --format=yaml \
   --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-dhe/pub-home-dhe-sealed-secrets.pem \
   --secret-file /home/dan/Git/kubelize/kube-projects/environments/home-dhe/inventory/secrets/secret.yaml \
   --sealed-secret-file /home/dan/Git/kubelize/kube-projects/environments/home-dhe/inventory/secrets/cf-api-token.yaml
+
+# nextcloud-mariadb
+kubeseal --format=yaml \
+  --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-dhe/pub-home-dhe-sealed-secrets.pem \
+  --secret-file /home/dan/Git/kubelize/kube-projects/environments/home-dhe/inventory/secrets/nextcloud-mariadb-secret.yaml \
+  --sealed-secret-file /home/dan/Git/kubelize/kube-projects/environments/home-dhe/inventory/secrets/nextcloud-mariadb-auth.yaml
+
+# nextcloud-redis
+kubeseal --format=yaml \
+  --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-dhe/pub-home-dhe-sealed-secrets.pem \
+  --secret-file /home/dan/Git/kubelize/kube-projects/environments/home-dhe/inventory/secrets/nextcloud-redis-secret.yaml \
+  --sealed-secret-file /home/dan/Git/kubelize/kube-projects/environments/home-dhe/inventory/secrets/nextcloud-redis-auth.yaml
